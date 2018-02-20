ecd87213a7917d00b08d320c4c256b2b4d05bd3c
Jenna Johnson <MillaJAB@users.noreply.github.com>
2018-02-19 21:18:58 -0600

    Removing cheat to auto-skip intro since spacebar skip added

--- js/imageLoading.js
+++ js/imageLoading.js
@@ -18,8 +18,7 @@ function countLoadedImagesAndLaunchIfReady() {
 	picsToLoad--;
 	// console.log(picsToLoad);
 	if(picsToLoad == 0) {
-		fadeInIntroThenStartGame(); //Uncomment this line and comment imageLoadingDoneSoStartGame to see intro before game loads
-		// imageLoadingDoneSoStartGame();
+		fadeInIntroThenStartGame();
 	}
 }
 
