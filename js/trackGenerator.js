4969a4f9822c00c57f2aaedd24dac0cdd9447eb3
McFunkypants <breakdance@mcfunkypants.com>
2018-02-22 16:24:34 -0800

    "valid race track" generator work in progress using a maze algorithm

--- js/trackGenerator.js
+++ js/trackGenerator.js
@@ -34,6 +34,9 @@ function generateRandomTrack(w,h) {
         }
     }
 
+    // maze generation lol
+    // mazeGeneration(data,w); // work in progress
+
     // horiz borders
     for (col=0; col<w; col++) {
             data[col+0*w] = TRACK_WALL;
@@ -69,4 +72,93 @@ function generateRandomTrack(w,h) {
     console.log('Finished generating random level of length: '+data.length);
 
     return data;
+}
+
+
+// Maze Generator, used in an old game of mine called Gates of Rath
+// Algorithm credit: http://www.roguebasin.roguelikedevelopment.org/index.php?title=Simple_maze
+function mazeGeneration(world, size) {
+    console.log('maze gen...');
+    // Coordinates of N(x,y) S(x,y) E(x,y) W(x,y)
+    // it's necessary to have one of each so that if one direction
+    // doesn't work properly, we can try each direction until one
+    // works, or that we know there isn't a location possible. 
+    var cN = [[0, 0], [0, 0], [0, 0], [0, 0]];
+    var x, y, cx, cy;
+    var randomDir, intDone = 0;
+    var map = new Array(size);
+    var failed = false; // avoid infinite loop
+    for (var i = 0; i <= size * size; ++i) {
+        map[i] = new Array(size);
+    }
+    // Initialize the Map Array to Zeros 
+    for (x = 1; x <= size; ++x) {
+        for (y = 1; y <= size; ++y) {
+            map[x][y] = 0;
+        } //end for
+    } //end for
+    var recursecount = 0;
+    do {
+        recursecount++;
+        if (recursecount % 25 == 0) console.log('Maze gen recursing ' + recursecount);
+        if (recursecount > 2000) {
+            console.log('MAZE GEN FAILED - took too long...');
+            failed = true; // no infinite loops, invalid is fine!
+            //return;
+        }
+        // Roll random x's and y's and make sure the value is odd
+        x = 2 + Math.floor(Math.random() * (size - 1)); if (x % 2 != 0)--x;
+        y = 2 + Math.floor(Math.random() * (size - 1)); if (y % 2 != 0)--y;
+        // Ensure that the first random map location starts the process
+        if (intDone == 0) map[x][y] = 1;
+        if (map[x][y] == 1) {
+            //Randomize Directions
+            randomDir = Math.floor(Math.random() * 4);
+            if (randomDir == 0) {
+                cN = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+            } else if (randomDir == 1) {
+                cN = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+            } else if (randomDir == 2) {
+                cN = [[0, -1], [0, 1], [-1, 0], [1, 0]];
+            } else if (randomDir == 3) {
+                cN = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+            } //end if
+            blnBlocked = 1;
+            do {
+                blnBlocked++;
+                for (var intDir = 0; intDir <= 3; ++intDir) {
+                    // Determine which direction the tile is
+                    cx = x + cN[intDir][0] * 2;
+                    cy = y + cN[intDir][1] * 2;
+                    //Check to see if the tile can be used
+                    if (cx < size && cy < size && cx > 1 && cy > 1) {
+                        if (map[cx][cy] != 1) {
+                            //create destination location
+                            map[cx][cy] = 1;
+                            //create current location
+                            map[x][y] = 1;
+                            //create inbetween location
+                            map[x + cN[intDir][0]][y + cN[intDir][1]] = 1;
+                            //set destination location to current
+                            x = cx; y = cy;
+                            blnBlocked = 0;
+                            intDone++;
+                            intDir = 4;
+                        } //end if
+                    } //end if
+                } //end for
+                //recursive, no directions found, loop back a node
+            } while (blnBlocked == 1) //end do
+        } //end if
+    } while (!failed && (intDone + 1 < ((size - 1) * (size - 1)) / 4)); //end do
+
+    // copy the data over in the format we need for the game
+    for (x = 1; x <= size; ++x) {
+        for (y = 1; y <= size; ++y) {
+            world[(x-1)+(y-1)*size] = map[x][y];
+        }
+    }
+
+    console.log('Maze Generation Complete!');
+
 }
