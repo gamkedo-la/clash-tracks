046118ceb62388a896e4d8ca84c2fe43e24f70e5
McFunkypants <breakdance@mcfunkypants.com>
2018-02-24 20:27:51 -0800

    level select screen

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -5,6 +5,7 @@ var menuDiv = document.getElementById('Menu');
 var helpDiv = document.getElementById('Help');
 var creditsDiv = document.getElementById('Credits');
 var optionsDiv = document.getElementById('Options');
+var levelDiv = document.getElementById('Levels');
 
 function mainMenu() {
     console.log('Main menu...');
@@ -12,20 +13,34 @@ function mainMenu() {
     helpDiv.style.display = 'none';
     creditsDiv.style.display = 'none';
     optionsDiv.style.display = 'none';
-
+    levelDiv.style.display = 'none';
 }
 
 function menuPlay() {
     console.log('Main menu: PLAY');
+    levelDiv.style.display = 'block';
     menuDiv.style.display = 'none';
     helpDiv.style.display = 'none';
     creditsDiv.style.display = 'none';
     optionsDiv.style.display = 'none';
+}
+
+function menuLevel(num) {
+    console.log('Main menu: PLAY LEVEL ' + num);
+    levelDiv.style.display = 'none';
+    menuDiv.style.display = 'none';
+    helpDiv.style.display = 'none';
+    creditsDiv.style.display = 'none';
+    optionsDiv.style.display = 'none';
+
+    window.level = num; // set the global
     fadeInIntroThenStartGame();
+
 }
 
 function menuHelp() {
     console.log('Main menu: HELP');
+    levelDiv.style.display = 'none';
     menuDiv.style.display = 'none';
     helpDiv.style.display = 'block';
     creditsDiv.style.display = 'none';
@@ -34,6 +49,7 @@ function menuHelp() {
 
 function menuCredits() {
     console.log('Main menu: CREDITS');
+    levelDiv.style.display = 'none';
     menuDiv.style.display = 'none';
     helpDiv.style.display = 'none';
     creditsDiv.style.display = 'block';
@@ -42,6 +58,7 @@ function menuCredits() {
 
 function menuOptions() {
     console.log('Main menu: OPTIONS');
+    levelDiv.style.display = 'none';
     menuDiv.style.display = 'none';
     helpDiv.style.display = 'none';
     creditsDiv.style.display = 'none';
