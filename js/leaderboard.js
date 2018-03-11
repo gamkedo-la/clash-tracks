a5f79b405106920bc64570384e241881044ca7ea
jeremyjackson89 <jeremy.jackson089@gmail.com>
2018-03-11 14:30:22 -0400

    Update leaderboard read and write.

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,5 +1,22 @@
-function updateLeaderboard(userName, score) {
-  firebase.database().ref('users/' + userName).set({
-    score: score
-  });
+function updateLeaderboard(level, userName, score) {
+    firebase.database().ref('levels/' + level + '/' + userName).set({
+        score: score
+    });
+}
+
+function getAllScores(callback) {
+	var endpoint = '/levels/'
+	getFromDatabase(endpoint, callback);
+}
+
+function getScoresForLevel(level, callback) {
+	var endpoint = '/levels/' + level
+	getFromDatabase(endpoint, callback);
+}
+
+function getFromDatabase(url, callback) {
+	firebase.database().ref(url).once('value').then(function(snapshot) {
+        var data = snapshot.val();
+        callback(data);
+    });
 }
